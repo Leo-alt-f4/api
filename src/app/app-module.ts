79ed06f09fd3adfe7a587d7b4f5c0ff3dfaf7a59
Leonardo Pereira Cabral Cunha <leonardo.cunha@intelbras.com.br>
2026-06-24 15:15:31 -0300

    3 - Ajusta os arquivos do tipo app

--- src/app/app-module.ts
+++ src/app/app-module.ts
@@ -1,6 +1,6 @@
 import { Module } from '@nestjs/common';
-import { PrismaModule } from './prisma/prisma-module';
-import { UsersModule } from './users/users-module';
+import { PrismaModule } from '../prisma/prisma-module';
+import { UsersModule } from '../users/users-module';
 
 @Module({
   imports: [PrismaModule, UsersModule],
