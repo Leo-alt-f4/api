92d9726c0985c204204f8adb59566cc5b2a87952
Leonardo Pereira Cabral Cunha <leonardo.cunha@intelbras.com.br>
2026-06-24 16:46:35 -0300

    5 - Fixa proteção de senhas de usuário com bcrypt e Ajusta a validação dos dados

--- src/users/users-service.ts
+++ src/users/users-service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from '../prisma/prisma-service';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class UsersService {
@@ -7,17 +8,34 @@ export class UsersService {
 
   async create(data: any) {
     const body = Array.isArray(data) ? data[0] : data;
+    const rounds = 10;
+    const hashedPassword = await bcrypt.hash(body.password, rounds);
 
     return this.prisma.user.create({
       data: {
         name: body.name,
         email: body.email,
-        password: body.password,
+        password: hashedPassword, 
       },
+
+      select: {
+        id: true,
+        name: true,
+        email: true,
+        password: true
+      }
     });
   }
 
   async findAll() {
-    return this.prisma.user.findMany();
+    return this.prisma.user.findMany({
+      select: {
+        id: true,
+        name: true,
+        email: true,
+        password: true,
+        tasks: true 
+      },
+    });
   }
 }
